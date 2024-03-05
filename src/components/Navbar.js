bb42e08b60dedf001f692f935543c6f7874851f9
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-05 13:31:02 +0530

    Submitted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,12 @@ import {Link, Outlet} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
+     <nav>
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
       </ul>
+      </nav>
       <Outlet/>
     </div>
   )
