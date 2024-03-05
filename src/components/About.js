f1eadb4167c0847ff076bf61c692c3e2ac0ab906
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-05 13:24:13 +0530

    Submit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>this is a simple react routing program</p>
+    </div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {BrowserRouter, Outlet, Route, Routes} from 'react-router-dom';
+import Home from "./Home";
+import About from "./About";
+import Navbar from "./Navbar";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+       <BrowserRouter>
+       <Navbar/>
+       
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+              <Route path="/about" element={<About/>}/>
+          
+        </Routes>
+       </BrowserRouter>
     </div>
   )
 }
